46e811565cd5319b53abb7700a2ba35813bb1509
robinrex92 <robinrex92@gmail.com>
2018-01-07 20:07:41 +0530

    Updated dependencies

--- FrostGlass/src/main/java/in/robinrex/frostglass/FrostTimeTracker.java
+++ FrostGlass/src/main/java/in/robinrex/frostglass/FrostTimeTracker.java
@@ -1,7 +1,10 @@
 package in.robinrex.frostglass;
 
 /**
- * @author ContusTeam <developers@contus.in>
+ * A simple benchmarking tool, to help optimize renderscript performance by tracking the time it takes to render each
+ * frame when frosting is in progress.
+ *
+ * @author Robin Rex G.
  * @version 1.0
  */
 
@@ -15,6 +18,9 @@ public class FrostTimeTracker {
 
     private static long mFrameTime = -1;
 
+    /**
+     * Resets the time tracker.
+     * */
     public static void reset() {
         mStartTime = -1;
         mProcessedFrames = 0;
@@ -22,10 +28,17 @@ public class FrostTimeTracker {
         mFrameTime = -1;
     }
 
+    /**
+     * Method that has to be called before calling invalidate on the view that is being frosted, or to be called
+     * before frosting any view or bitmap.
+     * */
     public static void start() {
         mStartTime = System.currentTimeMillis();
     }
 
+    /**
+     * Method to be called after invalidate() call on the view that is being frosted.
+     * */
     public static void frameComplete() {
 
         if (mStartTime == -1)
@@ -37,6 +50,13 @@ public class FrostTimeTracker {
         mProcessedFrames++;
     }
 
+    /**
+     * Method to get the average time it takes to frost a frame with any given settings to the renderscript library.
+     * This method has to be invoked after a considerable number of frost calls (or invalidate calls), to get an
+     * approximate aggregate of the frost times.
+     *
+     * @return Returns the average time it takes to frost a bitmap.
+     * */
     public static int getAverageTime() {
 
         if (mFrameTime == -1)
@@ -45,6 +65,11 @@ public class FrostTimeTracker {
         return (mTotalTime / mProcessedFrames);
     }
 
+    /**
+     * Method to print the average time it takes to frost a frame to be rendered. Same as {@link #getAverageTime()},
+     * it has to be called after a considerable number of frost calls (or invalidate calls), to print an approximate
+     * aggregate of the frost times.
+     * */
     public static void printAverageTime() {
         if (mFrameTime == -1)
             throw new IllegalStateException("FrostTimeTracker still running.");
